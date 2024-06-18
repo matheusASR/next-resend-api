caa0561758cdf00d8d772555a56727afbebdcc39
matheussASR <rm551466@fiap.com>
2024-06-18 12:57:32 -0300

    feat: adicionando repositories

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,5 +1,17 @@
 import emailRepository from "./email.repository";
+import senderRepository from "./sender.repository";
+import scheduleRepository from "./schedule.repository";
+import clientRepository from "./client.repository";
+import emailClassificationRepository from "./emailClassification.repository";
+import receiverRepository from "./receiver.repository";
+import campaignRepository from "./campaign.repository";
 
 export {
-    emailRepository
-}
+  emailRepository,
+  campaignRepository,
+  receiverRepository,
+  emailClassificationRepository,
+  clientRepository,
+  scheduleRepository,
+  senderRepository,
+};
