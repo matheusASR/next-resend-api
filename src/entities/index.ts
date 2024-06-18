79689d7d331fe13095eb3b08de2da2e0387bedec
matheussASR <rm551466@fiap.com>
2024-06-18 11:37:07 -0300

    feat: adicionando DER e entities index

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,3 +1,19 @@
 import { Email } from "./Email.entity";
+import { Client } from "./Client.entity";
+import { Campaign } from "./Campaign.entity";
+import { EmailClassification } from "./EmailClassification.entity";
+import { CampaignReceiver } from "./CampaignReceiver.entity";
+import { Receiver } from "./Receiver.entity";
+import { Schedule } from "./Schedule.entity";
+import { Sender } from "./Sender.entity";
 
-export { Email };
+export {
+  Email,
+  Client,
+  Campaign,
+  CampaignReceiver,
+  EmailClassification,
+  Receiver,
+  Schedule,
+  Sender,
+};
