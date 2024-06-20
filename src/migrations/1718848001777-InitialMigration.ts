74eab8bb51513e0fcb4968ea4eb0549280a9ad10
matheussASR <rm551466@fiap.com>
2024-06-19 22:48:00 -0300

    feat: rodando migrations

---
+++ src/migrations/1718848001777-InitialMigration.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1718848001777 implements MigrationInterface {
+    name = 'InitialMigration1718848001777'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "receivers" ("id" SERIAL NOT NULL, "email" character varying(255) NOT NULL, "name" character varying(255) NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_58db3b9ea40bf111b785247ea24" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "senders" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "alias" character varying(255) NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_398b8614004a406acf982651b46" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "email_classifications" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_24b65db1e60127d09ee3732aca6" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "schedules" ("id" SERIAL NOT NULL, "send_date" date NOT NULL, "send_hour" TIME NOT NULL, "status" character varying(50) NOT NULL, "error" text, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_7e33fc2ea755a5765e3564e66dd" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "clients" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "status" boolean NOT NULL DEFAULT true, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_f1ab7cf3a5714dbc6bb4e1c28a4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "emails" ("id" SERIAL NOT NULL, "title" character varying(255), "body" text, "subject" text, "image" bytea, "html_file" bytea, "receivers" text NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_a54dcebef8d05dca7e839749571" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "campaigns" ("id" SERIAL NOT NULL, "name" character varying(255) NOT NULL, "type" character varying(50) NOT NULL, "create_date" TIMESTAMP NOT NULL DEFAULT now(), "update_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_831e3fcd4fc45b4e4c3f57a9ee4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "campaign_receivers" ("id" SERIAL NOT NULL, CONSTRAINT "PK_dda664c64c77a8bb535da885bed" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "campaign_receivers"`);
+        await queryRunner.query(`DROP TABLE "campaigns"`);
+        await queryRunner.query(`DROP TABLE "emails"`);
+        await queryRunner.query(`DROP TABLE "clients"`);
+        await queryRunner.query(`DROP TABLE "schedules"`);
+        await queryRunner.query(`DROP TABLE "email_classifications"`);
+        await queryRunner.query(`DROP TABLE "senders"`);
+        await queryRunner.query(`DROP TABLE "receivers"`);
+    }
+
+}
---
+++ src/migrations/1718848044443-AddRelations.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddRelations1718848044443 implements MigrationInterface {
+    name = 'AddRelations1718848044443'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "schedules" ADD "emailId" integer`);
+        await queryRunner.query(`ALTER TABLE "schedules" ADD "receiverId" integer`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" ADD "campaignId" integer`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" ADD "receiverId" integer`);
+        await queryRunner.query(`ALTER TABLE "campaigns" ADD "clientId" integer`);
+        await queryRunner.query(`ALTER TABLE "email_classifications" ADD "campaignId" integer`);
+        await queryRunner.query(`ALTER TABLE "emails" ADD "classificationId" integer`);
+        await queryRunner.query(`ALTER TABLE "emails" ADD "senderId" integer`);
+        await queryRunner.query(`ALTER TABLE "schedules" ADD CONSTRAINT "FK_5d3d74bf8f1cfd954443b97d934" FOREIGN KEY ("emailId") REFERENCES "emails"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "schedules" ADD CONSTRAINT "FK_88deabec351e5c056342704c44d" FOREIGN KEY ("receiverId") REFERENCES "receivers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" ADD CONSTRAINT "FK_4f4233a07d61cee36a18b298d4a" FOREIGN KEY ("campaignId") REFERENCES "campaigns"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" ADD CONSTRAINT "FK_4033c19d70643df2dc6455ea375" FOREIGN KEY ("receiverId") REFERENCES "receivers"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "campaigns" ADD CONSTRAINT "FK_e61692e4fb96f6444339a1485e1" FOREIGN KEY ("clientId") REFERENCES "clients"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "email_classifications" ADD CONSTRAINT "FK_d556425d0b1a846b64aaa7814db" FOREIGN KEY ("campaignId") REFERENCES "campaigns"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "emails" ADD CONSTRAINT "FK_254ec2ceb2eb3922d754dff2e1e" FOREIGN KEY ("classificationId") REFERENCES "email_classifications"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "emails" ADD CONSTRAINT "FK_7346dd09401c768d4a64d420b7a" FOREIGN KEY ("senderId") REFERENCES "senders"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "emails" DROP CONSTRAINT "FK_7346dd09401c768d4a64d420b7a"`);
+        await queryRunner.query(`ALTER TABLE "emails" DROP CONSTRAINT "FK_254ec2ceb2eb3922d754dff2e1e"`);
+        await queryRunner.query(`ALTER TABLE "email_classifications" DROP CONSTRAINT "FK_d556425d0b1a846b64aaa7814db"`);
+        await queryRunner.query(`ALTER TABLE "campaigns" DROP CONSTRAINT "FK_e61692e4fb96f6444339a1485e1"`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" DROP CONSTRAINT "FK_4033c19d70643df2dc6455ea375"`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" DROP CONSTRAINT "FK_4f4233a07d61cee36a18b298d4a"`);
+        await queryRunner.query(`ALTER TABLE "schedules" DROP CONSTRAINT "FK_88deabec351e5c056342704c44d"`);
+        await queryRunner.query(`ALTER TABLE "schedules" DROP CONSTRAINT "FK_5d3d74bf8f1cfd954443b97d934"`);
+        await queryRunner.query(`ALTER TABLE "emails" DROP COLUMN "senderId"`);
+        await queryRunner.query(`ALTER TABLE "emails" DROP COLUMN "classificationId"`);
+        await queryRunner.query(`ALTER TABLE "email_classifications" DROP COLUMN "campaignId"`);
+        await queryRunner.query(`ALTER TABLE "campaigns" DROP COLUMN "clientId"`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" DROP COLUMN "receiverId"`);
+        await queryRunner.query(`ALTER TABLE "campaign_receivers" DROP COLUMN "campaignId"`);
+        await queryRunner.query(`ALTER TABLE "schedules" DROP COLUMN "receiverId"`);
+        await queryRunner.query(`ALTER TABLE "schedules" DROP COLUMN "emailId"`);
+    }
+
+}
